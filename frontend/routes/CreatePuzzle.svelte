b7f1571796c622ba88d67a4df9f185382aed350a
Jacob <kubap1402@gmail.com>
2022-12-16 21:24:58 +0100

    create puzzle form

--- frontend/routes/CreatePuzzle.svelte
+++ frontend/routes/CreatePuzzle.svelte
@@ -8,18 +8,24 @@
         //add puzzle to database
         //guessedCorrectly = await api.example.isMyUsername.query(username);
     }
+    let type: string = "0";
+
+    function print() {
+        console.log(type);
+    }
 </script>
 
 <p>Stwórz zadanie:</p>
 <form on:submit|preventDefault={create}>
-    <select>
-        <option>Znajdź Błąd</option>
-        <option>Napisz program</option>
-        <option>Uzupełnij Lukę</option>
-        <option>Co się wyświetli?</option>
+    <select bind:value={type} on:change={print}>
+        <option value="0">Znajdź Błąd</option>
+        <option value="1">Napisz program</option>
+        <option value="2">Uzupełnij Lukę</option>
+        <option value="3">Co się wyświetli?</option>
     </select>
+    <textarea />
 
     <input type="text" required bind:value={username} />
 
-    <button type="submit">Zgadnij</button>
+    <button type="submit">Wyslij</button>
 </form>
