788d30ae607d4ce78977b124ef341fd29595cbee
MrKamkar <kamilkarpiel0@gmail.com>
2022-12-17 08:02:31 +0100

    Naprawa headera

--- frontend/Header.svelte
+++ frontend/Header.svelte
@@ -19,7 +19,7 @@
     <div>
         <div class="data">
             <span class="code">let <span class="color">twoj_login</span> = <span class="color">"Czaros"</span>;</span><br>
-            <span class="code">let <span class="color">twoj_email</span> = <span class="color">"cezario@gmail.com"</span>;</span><br>
+            <span class="code">let <span class="color">twoj_email</span> = <span class="color">"cezario@gmail.com"</span>;</span>
         </div>
         <a href="/" use:link on:click={logOut}><button class="logout">Wyloguj się</button></a>
     </div>
@@ -35,14 +35,14 @@
         align-items: center;
         flex-wrap: wrap;
         padding: 2vw;
+    }
 
-        div {
-            width: 35%;
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            justify-content: space-between;
-        }
+    main > div {
+        width: 35vw;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
     }
 
     .name {
@@ -73,7 +73,7 @@
     .data .code {
         font-size: 0.9vw;
     }
-
+    
     .logout {
         color: $red-color;
     }
