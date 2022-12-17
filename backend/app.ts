8c700734499cd4d09a97d204e1a7041929da5384
Jacob <kubap1402@gmail.com>
2022-12-17 03:36:16 +0100

    updateRatings function

--- backend/app.ts
+++ backend/app.ts
@@ -124,14 +124,8 @@ async function main() {
                     oldPoints.puzzleRating,
                     puzzle.bugLine != input.line
                 );
-                await database.users.updateOne(
-                    { username },
-                    { $inc: { rating: newPoints.player } }
-                );
-                await database.puzzles.updateOne(
-                    { _id: new ObjectId(input.id) },
-                    { $inc: { rating: newPoints.puzzle } }
-                );
+                updateRatings(username, input.id, newPoints.player, newPoints.puzzle);
+
                 return {
                     ...oldPoints,
                     ...newPoints,
@@ -154,7 +148,10 @@ async function main() {
                 return false;
             }),
     });
-
+    async function updateRatings(username: string, id: string, player: number, puzzle: number) {
+        await database.users.updateOne({ username }, { $inc: { rating: player } });
+        await database.puzzles.updateOne({ _id: new ObjectId(id) }, { $inc: { rating: puzzle } });
+    }
     const app = express();
     app.use(express.static("frontend/public"));
     app.use("/trpc", makeTRPCExpressMiddleware(router));
